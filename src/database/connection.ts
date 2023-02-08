97dad57aa200ea2cfcf74ff198b6a11441b8bf39
tommy-skywalker  <babatomiwasokeyo@gmail.com>
2023-02-08 09:45:00 +0100

    Add MongoDB database connection module

---
+++ src/database/connection.ts
@@ -0,0 +1,53 @@
+import mongoose from 'mongoose';
+
+export class Database {
+  private static instance: Database;
+  private isConnected = false;
+
+  private constructor() {}
+
+  static getInstance(): Database {
+    if (!Database.instance) {
+      Database.instance = new Database();
+    }
+    return Database.instance;
+  }
+
+  async connect(): Promise<void> {
+    if (this.isConnected) {
+      console.log('Database already connected');
+      return;
+    }
+
+    try {
+      const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/shopswift';
+      
+      await mongoose.connect(mongoUri);
+      this.isConnected = true;
+      console.log('✅ MongoDB connected successfully');
+    } catch (error) {
+      console.error('❌ MongoDB connection error:', error);
+      throw error;
+    }
+  }
+
+  async disconnect(): Promise<void> {
+    if (!this.isConnected) {
+      return;
+    }
+
+    try {
+      await mongoose.disconnect();
+      this.isConnected = false;
+      console.log('MongoDB disconnected');
+    } catch (error) {
+      console.error('Error disconnecting from MongoDB:', error);
+      throw error;
+    }
+  }
+
+  getConnectionStatus(): boolean {
+    return this.isConnected;
+  }
+}
+
