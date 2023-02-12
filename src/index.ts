5c2326a3af852c865df1b53719e4ec8811e3d645
tommy-skywalker  <babatomiwasokeyo@gmail.com>
2023-02-12 16:45:00 +0100

    Setup Express server with middleware and basic routing

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import express, { Application } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+import productRoutes from './routes/product.routes';
+import userRoutes from './routes/user.routes';
+import orderRoutes from './routes/order.routes';
+
+dotenv.config();
+
+const app: Application = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(helmet());
+app.use(cors());
+app.use(morgan('combined'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
+
+// Health check endpoint
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'ok', message: 'ShopSwift API is running' });
+});
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route not found' });
+});
+
+// Error handler
+app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Something went wrong!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 ShopSwift server is running on port ${PORT}`);
+});
+
+export default app;
+
