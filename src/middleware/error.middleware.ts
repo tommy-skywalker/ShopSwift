c5b5d19cf903ac8cfd0f158ebef75067b6af4f14
tommy-skywalker  <babatomiwasokeyo@gmail.com>
2023-05-18 11:00:00 +0100

    Add centralized error handling middleware

---
+++ src/middleware/error.middleware.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from 'express';
+
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  console.error('Error:', err.message);
+  console.error('Stack:', err.stack);
+
+  res.status(500).json({
+    success: false,
+    error: process.env.NODE_ENV === 'production' 
+      ? 'Internal server error' 
+      : err.message
+  });
+};
+
+export const notFoundHandler = (req: Request, res: Response): void => {
+  res.status(404).json({
+    success: false,
+    error: `Route ${req.originalUrl} not found`
+  });
+};
+
