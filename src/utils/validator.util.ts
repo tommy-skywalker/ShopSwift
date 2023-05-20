a7c56c45a472390c04e54942a4595bb3f4847167
tommy-skywalker  <babatomiwasokeyo@gmail.com>
2023-05-20 10:45:00 +0100

    Add validation utility functions

---
+++ src/utils/validator.util.ts
@@ -0,0 +1,31 @@
+export class Validator {
+  static isEmail(email: string): boolean {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+
+  static isStrongPassword(password: string): boolean {
+    // At least 8 characters, one uppercase, one lowercase, one number
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d@$!%*?&]{8,}$/;
+    return passwordRegex.test(password);
+  }
+
+  static isPhoneNumber(phone: string): boolean {
+    const phoneRegex = /^\+?[\d\s-()]+$/;
+    return phoneRegex.test(phone) && phone.replace(/\D/g, '').length >= 10;
+  }
+
+  static isUrl(url: string): boolean {
+    try {
+      new URL(url);
+      return true;
+    } catch {
+      return false;
+    }
+  }
+
+  static sanitizeString(input: string): string {
+    return input.trim().replace(/[<>]/g, '');
+  }
+}
+
