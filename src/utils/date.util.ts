48082a83940fcf11ae7b1c0d01ec7a878f20007a
tommy-skywalker  <babatomiwasokeyo@gmail.com>
2023-07-02 13:00:00 +0100

    Add date and string utility functions

---
+++ src/utils/date.util.ts
@@ -0,0 +1,40 @@
+export class DateUtil {
+  static format(date: Date, format: string = 'YYYY-MM-DD HH:mm:ss'): string {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+
+    return format
+      .replace('YYYY', String(year))
+      .replace('MM', month)
+      .replace('DD', day)
+      .replace('HH', hours)
+      .replace('mm', minutes)
+      .replace('ss', seconds);
+  }
+
+  static addDays(date: Date, days: number): Date {
+    const result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
+  }
+
+  static addHours(date: Date, hours: number): Date {
+    const result = new Date(date);
+    result.setHours(result.getHours() + hours);
+    return result;
+  }
+
+  static isExpired(date: Date): boolean {
+    return date < new Date();
+  }
+
+  static getDaysDifference(date1: Date, date2: Date): number {
+    const diffTime = Math.abs(date2.getTime() - date1.getTime());
+    return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  }
+}
+
---
+++ src/utils/string.util.ts
@@ -0,0 +1,36 @@
+export class StringUtil {
+  static capitalize(str: string): string {
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+  }
+
+  static slugify(str: string): string {
+    return str
+      .toLowerCase()
+      .trim()
+      .replace(/[^\w\s-]/g, '')
+      .replace(/[\s_-]+/g, '-')
+      .replace(/^-+|-+$/g, '');
+  }
+
+  static truncate(str: string, length: number, suffix: string = '...'): string {
+    if (str.length <= length) return str;
+    return str.substring(0, length - suffix.length) + suffix;
+  }
+
+  static randomString(length: number = 10): string {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      result += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return result;
+  }
+
+  static maskEmail(email: string): string {
+    const [local, domain] = email.split('@');
+    if (!domain) return email;
+    const maskedLocal = local.substring(0, 2) + '***' + local.substring(local.length - 1);
+    return `${maskedLocal}@${domain}`;
+  }
+}
+
