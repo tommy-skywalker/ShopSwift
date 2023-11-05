30be585e099e7fc87d187415a87dbc1acbbe6dfa
tommy-skywalker  <babatomiwasokeyo@gmail.com>
2023-11-05 15:30:00 +0100

    Add TypeScript type definitions for API responses

---
+++ src/types/index.ts
@@ -0,0 +1,23 @@
+export type HttpStatusCode = 200 | 201 | 400 | 401 | 403 | 404 | 500;
+
+export interface ApiResponse<T = any> {
+  success: boolean;
+  data?: T;
+  error?: string;
+  message?: string;
+}
+
+export interface PaginationParams {
+  page?: number;
+  limit?: number;
+}
+
+export interface PaginatedResponse<T> extends ApiResponse<T[]> {
+  pagination?: {
+    page: number;
+    limit: number;
+    total: number;
+    totalPages: number;
+  };
+}
+
